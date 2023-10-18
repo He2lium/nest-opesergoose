1a98f7221ee8fb1ef49693f24a7ff24cf705c654
he2lium <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>
2023-10-18 15:08:22 +0700

    OPESER_PLUGIN_CONNECTOR_TOKEN

--- lib/index.ts
+++ lib/index.ts
@@ -1,6 +1,6 @@
 import {OpeserModule} from "./opeser.module";
 import {OpeserService} from "./opeser.service";
-import {OPESER_CLIENT_TOKEN, OPESER_PLUGIN_TOKEN} from "./opeser.constants"
+import {OPESER_CLIENT_TOKEN, OPESER_PLUGIN_CONNECTOR_TOKEN} from "./opeser.constants"
 import {OgProp} from "./decorators/opeser-prop.decorator";
 import {InjectOpeser} from "./decorators/opeser-client.decorator";
 import {OgSchema} from "./decorators/opeser-schema.decorator";
@@ -16,7 +16,7 @@ export {
     OpeserService,
     OpeserMappingStorage,
     OPESER_CLIENT_TOKEN,
-    OPESER_PLUGIN_TOKEN,
+    OPESER_PLUGIN_CONNECTOR_TOKEN,
     OgProp,
     InjectOpeser,
     OgSchema,
