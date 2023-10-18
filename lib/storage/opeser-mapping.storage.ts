0f0c3b7c970562beefa5820162c552e3f800e87c
pavelprna <pavel@prna.ru>
2023-10-18 14:38:10 +0700

    fix: mapping

--- lib/storage/opeser-mapping.storage.ts
+++ lib/storage/opeser-mapping.storage.ts
@@ -1,33 +1,36 @@
-import {OpeserStorageType} from "../types/opeser-storage.type";
+import { OpeserStorageType } from "../types/opeser-storage.type";
 import field = OpeserStorageType.field;
 import schema = OpeserStorageType.schema;
 
 export class OpeserMappingStorageClass {
-    private schemas: schema[] = []
-    private fields: field[] = []
+  private schemas: schema[] = [];
+  private fields: field[] = [];
 
-    setProp(field: field) {
-        this.fields.push(field)
-    }
+  setProp(field: field) {
+    this.fields.push(field);
+  }
 
-    getProps(className: string) {
-        return this.fields.filter((field) => field.parent === className)
-    }
+  getProps(className: string) {
+    return this.fields.filter((field) => field.parent === className);
+  }
 
-    getSchemaByClass(className: string) {
-        return this.schemas.find((schema) => schema.class === className)
-    }
+  getSchemaByClass(className: string) {
+    return this.schemas.find((schema) => schema.class === className);
+  }
 
-    getSchemaByIndex(indexName: string) {
-        return this.schemas.find((schema) => schema.index === indexName)
-    }
+  getSchemaByIndex(indexName: string) {
+    return this.schemas.find((schema) => schema.index === indexName);
+  }
 
-    setSchema(schema: schema) {
-        this.schemas.push(schema)
-    }
+  setSchema(schema: schema) {
+    this.schemas.push(schema);
+  }
 }
 
 // Global es-map storage
-const globalRef = global as any
+const globalRef = global as typeof globalThis & {
+  OpeserMappingStorage: OpeserMappingStorageClass;
+};
 export const OpeserMappingStorage =
-    globalRef.OpeserMappingStorage || (globalRef.OpeserMappingStorage = new OpeserMappingStorageClass())
+  globalRef.OpeserMappingStorage ||
+  (globalRef.OpeserMappingStorage = new OpeserMappingStorageClass());
--- lib/utils/opeser-plugin-option-factory.util.ts
+++ lib/utils/opeser-plugin-option-factory.util.ts
@@ -1,12 +1,11 @@
-import {OpeserOptions} from "opesergoose";
-import {OpeserMappingStorage} from "../storage/opeser-mapping.storage";
+import { OpeserOptions } from "opesergoose";
+import { OpeserMappingStorage } from "../storage/opeser-mapping.storage";
 
-export const OpeserPluginOptionFactory =
-    (
-        index: string,
-        options?: Omit<OpeserOptions.PluginOptions<{}>, "index"|"mapProperties">
-    ):OpeserOptions.PluginOptions<{}> =>({
-        ...(options??{}),
-        index,
-        mapProperties: OpeserMappingStorage.getSchemaByIndex(index)
-    })
+export const OpeserPluginOptionFactory = (
+  index: string,
+  options: Omit<OpeserOptions.PluginOptions<{}>, "index" | "mapProperties"> = {}
+): OpeserOptions.PluginOptions<{}> => ({
+  ...options,
+  index,
+  mapProperties: OpeserMappingStorage.getSchemaByIndex(index).map,
+});
