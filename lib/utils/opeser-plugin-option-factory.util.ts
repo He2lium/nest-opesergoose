39bce4921af33791555108e57733c36611b9ec83
he2lium <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>
2023-10-18 14:12:13 +0700

    OpeserPluginOptionFactory

--- lib/utils/opeser-plugin-option-factory.util.ts
+++ lib/utils/opeser-plugin-option-factory.util.ts
@@ -2,10 +2,10 @@ import {OpeserOptions} from "opesergoose";
 import {OpeserMappingStorage} from "../storage/opeser-mapping.storage";
 
 export const OpeserPluginOptionFactory =
-    <DocumentType>(
+    (
         index: string,
-        options?: Omit<OpeserOptions.PluginOptions<DocumentType>, "index"|"mapProperties">
-    ):OpeserOptions.PluginOptions<DocumentType> =>({
+        options?: Omit<OpeserOptions.PluginOptions<{}>, "index"|"mapProperties">
+    ):OpeserOptions.PluginOptions<{}> =>({
         ...(options??{}),
         index,
         mapProperties: OpeserMappingStorage.getSchemaByIndex(index)
