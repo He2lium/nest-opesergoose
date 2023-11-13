94aee551c1b83deb376f677980c1ebdc25656281
pavelprna <pavel@prna.ru>
2023-11-13 11:14:02 +0700

    feat: virtual inndex settings

--- lib/decorators/opeser-schema.decorator.ts
+++ lib/decorators/opeser-schema.decorator.ts
@@ -6,15 +6,24 @@ import { OpeserStorageType } from "../types/opeser-storage.type";
 import field = OpeserStorageType.field;
 import { GetFiledMapUtil } from "../utils/get-filed-map.util";
 import { OpeserMappingStorage } from "../storage/opeser-mapping.storage";
+import merge from "lodash/merge";
 
-export const OgSchema = (index?: string, settings?: IndicesIndexSettings) => {
+export const OgSchema = (
+  index?: string,
+  settings: IndicesIndexSettings = {}
+) => {
   return function (target: any) {
     const fields: field[] = OpeserMappingStorage.getProps(target.name);
     const mapping: Record<string, MappingProperty> = {};
+
     for (let field of fields) {
-      mapping[field.key] = GetFiledMapUtil(field.options);
-      if (field.virtual)
-        mapping[`_${field.key}`] = GetFiledMapUtil(field.virtual);
+      mapping[field.key] = GetFiledMapUtil(field.options).map;
+      if (field.virtual) {
+        const { map, analysis } = GetFiledMapUtil(field.virtual);
+
+        mapping[`_${field.key}`] = map;
+        settings.analysis = merge(settings.analysis, analysis);
+      }
     }
     OpeserMappingStorage.setSchema({
       index,
--- lib/utils/get-filed-map.util.ts
+++ lib/utils/get-filed-map.util.ts
@@ -1,19 +1,22 @@
-import {MappingProperty} from "@opensearch-project/opensearch/api/types";
-import {MappingPropertyBaseWithPropertiesFromClass} from "../types/opeser-decorator.type";
-import {OpeserStorageType} from "../types/opeser-storage.type";
+import { MappingProperty } from "@opensearch-project/opensearch/api/types";
+import { MappingPropertyBaseWithPropertiesFromClass } from "../types/opeser-decorator.type";
+import { OpeserStorageType } from "../types/opeser-storage.type";
 import schema = OpeserStorageType.schema;
-import {OpeserMappingStorage} from "../storage/opeser-mapping.storage";
+import { OpeserMappingStorage } from "../storage/opeser-mapping.storage";
 
 export function GetFiledMapUtil(options: MappingProperty) {
-    if ((options as MappingPropertyBaseWithPropertiesFromClass).propertiesFromClass) {
-        const nestedSchema: schema = OpeserMappingStorage.getSchemaByClass(
-            (options as MappingPropertyBaseWithPropertiesFromClass).propertiesFromClass.name,
-        )
-        const map = {
-            ...options,
-            properties: nestedSchema.map,
-        }
-        delete map['propertiesFromClass']
-        return map
-    } else return options
+  if (
+    (options as MappingPropertyBaseWithPropertiesFromClass).propertiesFromClass
+  ) {
+    const nestedSchema: schema = OpeserMappingStorage.getSchemaByClass(
+      (options as MappingPropertyBaseWithPropertiesFromClass)
+        .propertiesFromClass.name
+    );
+    const map = {
+      ...options,
+      properties: nestedSchema.map,
+    };
+    delete map["propertiesFromClass"];
+    return { map, analysis: nestedSchema.settings.analysis };
+  } else return { map: options };
 }
