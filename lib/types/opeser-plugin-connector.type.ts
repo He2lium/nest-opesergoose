409d7215106215757ad39ea4d31dea0e4807bbc2
pavelprna <pavel@prna.ru>
2024-01-23 18:28:04 +0700

    fix: remove opesergoose

--- lib/types/opeser-plugin-connector.type.ts
+++ lib/types/opeser-plugin-connector.type.ts
@@ -1,8 +1,7 @@
 import { Schema } from "mongoose";
-import { OpeserOptions } from "opesergoose";
 
 export type OpeserPluginConnector = (
   index: string,
   schema: Schema,
-  options: Omit<OpeserOptions.PluginOptions, "index" | "mapProperties">
+  options,
 ) => void;
